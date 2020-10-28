794d9dc40336de7f4b3b94c49702871ae3737ac6
Pachy Hugo <lapach@MacBook-Pro-de-Pachy.local>
2020-10-28 11:41:03 +0100

    delete useless file

--- toto.js
+++
@@ -1,1 +0,0 @@
-import*as _ from"lodash";import{cube}from"./math";function component(){const e=document.createElement("pre");return e.innerHTML=["Hello webpack!","5 cubed is equal to "+cube(5)].join("\n\n"),e}"production"!==process.env.NODE_ENV&&console.log("Looks like we are in development mode!"),document.body.appendChild(component());
